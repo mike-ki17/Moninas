39e03680042912c569ce25553ce0a79ec4c264b0
Mike <michael16cd@gmail.com>
2024-07-31 13:59:00 -0500

    Menu movil, no estable

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,25 +6,87 @@ import { useCarContext } from "../../context/showCar";
 import { useProductContext } from "../../context/productContext.jsx";
 import { BiMenuAltRight } from "react-icons/bi";
 import "./Nav.css";
-function Nav({ photo, logOut, name, display}) {
-
+import { useState } from "react";
+function Nav({ photo, logOut, name, display }) {
   const { showCar, handleCar } = useCarContext();
-  const {listProduct} = useProductContext()
+  const { listProduct } = useProductContext();
+  const [showMenuMovil, setShowMenuMovil] = useState(false);
+
+  const handleMenu = () => {
+    showMenuMovil ? setShowMenuMovil(false) : setShowMenuMovil(true);
+  };
 
   return (
     <div className="container-nav">
       <nav className="nav-movile">
         <ul className="nav-list">
-           <img src={Logo} alt="Moninas" className="logo" />
+          <li>
+            <img src={Logo} alt="Moninas" className="logo" />
+          </li>
         </ul>
         <ul className="nav-funcion">
           <button className="container-btn-car" onClick={handleCar}>
             <BiCartAlt className="btn-car" />
-            <span className={listProduct.length == 0 ? "not-counter-car" : "counter-car"}>{listProduct.length}</span>
+            <span
+              className={
+                listProduct.length == 0 ? "not-counter-car" : "counter-car"
+              }
+            >
+              {listProduct.length}
+            </span>
           </button>
-          <BiMenuAltRight className="btn-menu"/>
+          <BiMenuAltRight className="btn-menu" onClick={handleMenu} />
+          <img
+            src={photo}
+            alt=""
+            className={display ? "user-session" : "photo-user"}
+          />
         </ul>
       </nav>
+
+      <div className={showMenuMovil ? "display_menu" : "ocult_menu"}>
+        <div className="routes">
+          <ul className="nav-list-movil">
+            <li>
+              <NavLink to="/" className="link-movil">
+                Inicio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/productos" className="link-movil">
+                Productos
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacto" className="link-movil">
+                Contacto
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="functions">
+          <ul className="nav-funcion-menuMovile">
+          <NavLink to="/login" className={display ? "log-in" : "user-session"}>
+            Log in
+          </NavLink>
+          <NavLink
+            to="/register"
+            className={display ? "sing-up" : "user-session"}
+          >
+            Sing up
+          </NavLink>
+          
+          <NavLink
+            to="/login"
+            className={display ? "user-session" : "sing-up"}
+            onClick={logOut}
+          >
+            log out
+          </NavLink>
+          
+        </ul>
+        </div>
+      </div>
       <nav className="nav-principal">
         <ul className="nav-list">
           <img src={Logo} alt="Moninas" className="logo" />
@@ -45,29 +107,41 @@ function Nav({ photo, logOut, name, display}) {
           </li>
         </ul>
         <ul className="nav-funcion">
-          <NavLink to="/login" className={display ? "log-in" : 'user-session'}>
+          <NavLink to="/login" className={display ? "log-in" : "user-session"}>
             Log in
           </NavLink>
-          <NavLink to="/register" className={display ? "sing-up" : 'user-session'}>
+          <NavLink
+            to="/register"
+            className={display ? "sing-up" : "user-session"}
+          >
             Sing up
           </NavLink>
-          <img src={photo} alt="" className={display ? "user-session" : 'photo-user'} />
-          <p className={display ? "user-session" : 'log-in'}>{name}</p>
-          <NavLink to="/login" className={display ? "user-session" : 'sing-up'} onClick={logOut}>
+          <img
+            src={photo}
+            alt=""
+            className={display ? "user-session" : "photo-user"}
+          />
+          <p className={display ? "user-session" : "log-in"}>{name}</p>
+          <NavLink
+            to="/login"
+            className={display ? "user-session" : "sing-up"}
+            onClick={logOut}
+          >
             log out
           </NavLink>
           <button className="container-btn-car" onClick={handleCar}>
             <BiCartAlt className="btn-car" />
-            <span className={listProduct.length == 0 ? "not-counter-car" : "counter-car"}>{listProduct.length}</span>
+            <span
+              className={
+                listProduct.length == 0 ? "not-counter-car" : "counter-car"
+              }
+            >
+              {listProduct.length}
+            </span>
           </button>
-         
         </ul>
       </nav>
-      {
-        showCar && <Car />
-      }
-     
-     
+      {showCar && <Car />}
     </div>
   );
 }
