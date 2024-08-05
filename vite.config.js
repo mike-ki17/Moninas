bdfbfc7bb16ac316efebcf180582e1f34eaf768e
Mike <michael16cd@gmail.com>
2024-08-05 13:40:25 -0500

    Published, modify vite.config and package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/mike-ki17/Moninas',
   build: {
     sourcemap: true, 
   },
