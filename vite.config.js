719294c89d355b7250734a1aa7701e4095ea8874
Mike <michael16cd@gmail.com>
2024-08-05 13:50:16 -0500

    Corrección enlace de pubicación

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/mike-ki17/Moninas',
+  base: 'https://mike-ki17.github.io/Moninas',
   build: {
     sourcemap: true, 
   },
