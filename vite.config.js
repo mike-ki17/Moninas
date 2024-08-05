c9a32dc55d0da4eed7c5af193c5338779ca554ce
Mike <michael16cd@gmail.com>
2024-08-05 13:55:19 -0500

    Configuración de la base -> archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://mike-ki17.github.io/Moninas',
-  build: {
-    sourcemap: true, 
-  },
+  base: "https://mike-ki17.github.io/Moninas/",
+  // build: {
+  //   sourcemap: true, 
+  // },
 })
